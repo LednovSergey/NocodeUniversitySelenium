673fe23ef5e14e5f81e2ca2bba1158edabd4472e
LednovSergey <tema932@gmail.com>
2023-10-07 12:56:36 +0200

    Commit

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,73 @@
-package PACKAGE_NAME;public class BaseTest {
+import io.qameta.allure.Attachment;
+import org.junit.After;
+import org.junit.AssumptionViolatedException;
+import org.junit.Before;
+import org.junit.jupiter.api.*;
+import org.junit.Rule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.Random;
+
+
+public class BaseTest {
+    WebDriver driver;
+
+    protected String BASE_URL = "https://jere237.softr.app/";
+
+    @Before
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sergey\\OneDrive\\Рабочий стол\\Drive\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(BASE_URL);
+    }
+
+    @Rule
+    public TestWatcher screenShotFailure = new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, Description description) {
+            makeScreenshotOnFailure();
+            driver.close();
+            driver.quit();
+        }
+
+        @Override
+        protected void succeeded(Description description) {
+            driver.close();
+            driver.quit();
+        }
+
+        @Override
+        protected void skipped(AssumptionViolatedException e, Description description) {
+            makeScreenshotOnFailure();
+            driver.close();
+            driver.quit();
+        }
+
+        @Attachment
+        public byte[] makeScreenshotOnFailure(){
+            return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+        }
+    };
+
+    public static String getRandomEmail() {
+        String SALTCHARS = "abcdefghijklmnopqrstufwxyz1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 20) {
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr + "@gmail.com";
+    }
 }
